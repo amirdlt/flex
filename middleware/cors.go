c745bef09b74e68bedcd9adb5407dc03d4f1461b
AmirhosseinDolatkhah <AmirhosseinDolatkhah2000@gmail.com>
2023-05-09 13:04:43 +0330

    [feat] add cors handler middleware

---
+++ middleware/cors.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	. "github.com/amirdlt/flex"
+	"net/http"
+)
+
+func CORS[I Injector]() Wrapper[I] {
+	return func(inner Handler[I]) Handler[I] {
+		return func(i I) Result {
+			result := inner(i)
+
+			i.ResponseHeaders().Set("Access-Control-Allow-Origin", "*")
+			i.ResponseHeaders().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+			i.ResponseHeaders().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+			if i.Method() == http.MethodOptions {
+				result = i.WrapNoContent()
+			}
+
+			return result
+		}
+	}
+}
