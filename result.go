c8e7ffdc00d37467eea27e6c4f2443104f93bbd1
AmirhosseinDolatkhah <AmirhosseinDolatkhah2000@gmail.com>
2023-05-14 15:22:33 +0330

    [fix] minor issue in flex.Result

--- result.go
+++ result.go
@@ -29,7 +29,7 @@ func (r Result) GetExtValue() any {
 }
 
 func (r Result) IsSuccessful() bool {
-	return r.statusCode != 0 && r.statusCode < 300 && r.statusCode >= 200
+	return r.statusCode == 0 || r.statusCode < 300 && r.statusCode >= 200
 }
 
 func (r Result) IsTerminated() bool {
